1565463010b86bf29b33cbf70d3b2a100abcab91
pramebt <bdforwk@gmail.com>
2025-04-16 22:14:10 +0700

    update router

---
+++ controllers/camping.js
@@ -0,0 +1,43 @@
+exports.listCamping = (req,res) =>{
+    try{
+        console.log('Hello controllers')
+        console.log(asdsa)
+    res.send('Hello controllers')
+    } catch(error){
+        console.log(error.message)
+        res.status(500).json({message:'Server Error'})
+    }
+    
+};
+
+exports.readCamping = (req,res)=>{
+    try{
+        res.send('Hello READ')
+    } catch(error){
+        res.status(500).json({message:'Server Error'})
+    }
+};
+
+exports.createCamping = (req,res)=>{
+    try{
+        res.send('Hello Create')
+    } catch(error){
+        res.status(500).json({message:'Server Error'})
+    }
+};
+
+exports.updateCamping = (req,res)=>{
+    try{
+        res.send('Hello Update')
+    } catch(error){
+        res.status(500).json({message:'Server Error'})
+    }
+};
+
+exports.deleteCamping = (req,res)=>{
+    try{
+        res.send('Hello Delete')
+    } catch(error){
+        res.status(500).json({message:'Server Error'})
+    }
+};
--- routes/camping.js
+++ routes/camping.js
@@ -1,40 +1,33 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+
+//controllers
+const { listCamping ,
+     readCamping ,
+      createCamping ,
+       updateCamping ,
+        deleteCamping} = require('../controllers/camping');
 
 
 //@ENDPOINT http://localhost:5000/api/camping/2
 //@METHODE GET [list]
 //@ACCESS PUBLIC
-router.get('/camping',(req,res)=>{
-    res.send('Hello route')
-})
+router.get('/camping',listCamping);
 //@ENDPOINT http://localhost:5000/api/camping
 //@METHODE GET [read]
 //@ACCESS PUBLIC
-router.get('/camping/:id',(req,res)=>{
-    res.send('Hello route')
-})
+router.get('/camping/:id',readCamping)
 //@ENDPOINT http://localhost:5000/api/camping
 //@METHODE POST [create]
 //@ACCESS PRIVEATE
-router.post('/camping',(req,res)=>{
-    const {title,price} = req.body
-    res.send("Hello post camping")
-    console.log(title)
-    console.log(price)
-})
+router.post('/camping', createCamping)
 //@ENDPOINT http://localhost:5000/api/camping
 //@METHODE Put [edit]
 //@ACCESS PRIVEATE
-router.put('/camping/:id',(req,res)=>{
-    res.send("Hello put camping")
-    console.log(req.params.id)
-})
+router.put('/camping/:id',updateCamping)
 //@ENDPOINT http://localhost:5000/api/camping
 //@METHODE DELETE [del]
 //@ACCESS PRIVEATE
-router.delete('/camping/:id',(req,res)=>{
-    res.send("Hello DELETE")
-})
+router.delete('/camping/:id',deleteCamping)
 
 module.exports = router
