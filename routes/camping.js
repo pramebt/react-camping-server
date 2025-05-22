0465661bbab86d5c115dfaf46f764de7b91c0bd9
pramebt <bdforwk@gmail.com>
2025-05-22 13:40:23 +0700

    del comment

--- routes/camping.js
+++ routes/camping.js
@@ -9,25 +9,15 @@ const { listCamping ,
         deleteCamping} = require('../controllers/camping');
 
 const { authCheck} = require('../middlewares/auth')
-//@ENDPOINT http://localhost:5000/api/camping/2
-//@METHODE GET [list]
-//@ACCESS PUBLIC
+
 router.get('/camping',listCamping);
-//@ENDPOINT http://localhost:5000/api/camping
-//@METHODE GET [read]
-//@ACCESS PUBLIC
+
 router.get('/camping/:id',readCamping)
-//@ENDPOINT http://localhost:5000/api/camping
-//@METHODE POST [create]
-//@ACCESS PRIVEATE
+
 router.post('/camping',authCheck,createCamping)
-//@ENDPOINT http://localhost:5000/api/camping
-//@METHODE Put [edit]
-//@ACCESS PRIVEATE
+
 router.put('/camping/:id',updateCamping)
-//@ENDPOINT http://localhost:5000/api/camping
-//@METHODE DELETE [del]
-//@ACCESS PRIVEATE
+
 router.delete('/camping/:id',deleteCamping)
 
 module.exports = router
