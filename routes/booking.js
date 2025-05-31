0a32d460fcb1bbf444a4d01302fec362a86a41fd
pramebt <bdforwk@gmail.com>
2025-06-01 01:38:32 +0700

    add route

--- routes/booking.js
+++ routes/booking.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
 //constrollers
-const { createBooking } = require("../controllers/booking.js")
+const { createBooking ,checkout} = require("../controllers/booking.js")
 const { authCheck } = require("../middlewares/auth")
 //@endpoint http://localhost:5000/api/booking
 router.post('/booking',authCheck,createBooking)
 
-
+router.post('/checkout',authCheck,checkout)
 module.exports = router
